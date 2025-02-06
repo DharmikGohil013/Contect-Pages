a967f27cdd59cb02c49ed19e647e94713629f4df
DHARMIKgohil013 <dharmikgohil395003@gmail.com>
2025-02-06 15:30:44 +0530

    add

---
+++ react-contact-form-main/src/App.jsx
@@ -0,0 +1,15 @@
+import ContactForm from './ContactForm';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>React Contact Form</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
